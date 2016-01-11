764921cdda538fafff108044566ac10fc197d265
usama ahmed <uusa35@gmail.com>
2016-01-11 17:37:00 +0300

    updating

--- app/Http/Controllers/Backend/InvoiceController.php
+++ app/Http/Controllers/Backend/InvoiceController.php
@@ -6,6 +6,7 @@ use App\Core\CreateExcelSheet;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Response;
+use Laracasts\Flash\Flash;
 
 class InvoiceController extends Controller
 {
